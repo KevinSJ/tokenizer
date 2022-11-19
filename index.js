091a413e6bd04f537d3602a36322f33aa7141448
Kevin Jiang <KevinSJ@users.noreply.github.com>
2022-11-19 22:12:52 +1300

    linting update

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-import express from 'express';
-import controllers from './src/controllers/index.js';
+import express from "express";
+import controllers from "./src/controllers/index.js";
 
 const app = express();
 const PORT = process.env.PORT ?? 3000;
@@ -7,6 +7,8 @@ const PORT = process.env.PORT ?? 3000;
 app.use(express.json());
 app.use(controllers);
 
-app.listen(PORT, () => {
-  console.log('listen on port 3000');
+const server = app.listen(PORT, () => {
+  console.debug("Listen on port " + PORT);
 });
+
+export default server;
